c01a4086535850f5412228cfb9134781051ecb33
bamdal <alstn7695@gmail.com>
2024-06-19 14:38:23 +0900

    오브젝트 rotation에따라 잘리게 적용 완료

--- Assets/_Scripts/Slice/SliceObject.cs
+++ Assets/_Scripts/Slice/SliceObject.cs
@@ -27,9 +27,26 @@ public class SliceObject : Slice
     public void SliceMesh(Plane plane)
     {
 
-        Vector3 planeNormal = transform.TransformDirection(plane.normal);
+        // 회전 적용 값
+        Quaternion rotationToTarget = Quaternion.FromToRotation(transform.forward, Vector3.forward);
+        rotationToTarget *= Quaternion.FromToRotation(transform.right, Vector3.right);
+        rotationToTarget *= Quaternion.FromToRotation(transform.up, Vector3.up);
+
+        // 월드기준으로 변환후 회전한 만큼 변화
+        Vector3 planeNormal = rotationToTarget * transform.TransformDirection(plane.normal);
+
         float planeDistance = plane.distance + Vector3.Dot(plane.normal, transform.position);
-        Plane worldPlane = new Plane(planeNormal, planeDistance);
+                Plane worldPlane = new Plane(planeNormal, planeDistance);
+
+/*        Vector3 planeNormal = transform.TransformDirection(plane.normal);
+        float planeDistance = Vector3.Dot(transform.TransformPoint(plane.normal * plane.distance), planeNormal);
+        Plane worldPlane = new Plane(planeNormal, planeDistance);*/
+    
+        // 평면을 월드 좌표계로 변환하고 스케일 조정
+        /*        Vector3 planeNormal = Vector3.Scale(transform.TransformDirection(plane.normal), transform.lossyScale).normalized;
+                float planeDistance = Vector3.Dot(transform.TransformPoint(plane.normal * plane.distance), planeNormal);
+                Plane worldPlane = new Plane(planeNormal, planeDistance);*/
+
         Mesh[] meshes = Slicer(Filter, worldPlane);
         // 잘린 매쉬 중에 1 번은 범위 안에 있는 오브젝트
         if (meshes != null)
